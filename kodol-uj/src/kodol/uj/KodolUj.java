211d348663a31c597e1972048471973c8658db52
Szemere Gábor <szeg2001@gmail.com>
2020-03-27 18:28:26 +0100

    Update KodolUj.java
    Encoding changed, set used

--- kodol-uj/src/kodol/uj/KodolUj.java
+++ kodol-uj/src/kodol/uj/KodolUj.java
@@ -5,7 +5,9 @@
  */
 package kodol.uj;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  *
@@ -17,22 +19,33 @@ public class KodolUj {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        //WINDOWS-1252 encoding addad, because Scenner uses operating system character coding, SzeG
+        Scanner sc = new Scanner(System.in, "Windows-1252");
         String nyilt;
         System.out.println("Kérem adjon meg egy maximum 255 karakter hosszúságú szöveget!");
-        nyilt=sc.nextLine();
+
+        //nyilt="kérem dg";
+        nyilt = sc.nextLine();
         //SZÓKÖZELTÁVOLÍTÓ
-        nyilt.trim();
+        nyilt = nyilt.trim();
+
         //NAGYBETŰTELENÍTŐ
-        nyilt.toUpperCase();
+        nyilt = nyilt.toUpperCase();
         System.out.println(nyilt);
+// Creating a Set of Characters; adding characters from A to Z to the set. SzeG
+        Set jobetuk = new HashSet();
+        for (char i = 'A'; i <= 'Z'; i++) {
+            jobetuk.add(i);
+        }
+        System.out.println(jobetuk.contains('A') ); //returns true, becuse set contains character A
+        System.out.println(jobetuk.contains('j'));//returns false, becouse set does'nt contain xharacter j
+        //  List<Character>lista=new ArrayList<>();
         //MEGINT UGYANAZ A PROBLÉMA, NEM TESZI A DOLGÁT, van szóköz, van kisbetű, ellenben az ékezetes betűk felismerhetetlenné válnak.
         /*for (int i = 0; i < 255; i++) {
             if (nyilt.charAt(i)) {
                 
             }
         }*/
-      
     }
-    
+
 }
